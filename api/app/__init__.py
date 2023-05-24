0977d2e8aeac2bdfdafe20eec259be48238c3443
Rahul Kumar S <86195461+HLrahul@users.noreply.github.com>
2023-05-24 08:04:01 +0000

    ˋcontainerizedˋ the project

---
+++ api/app/__init__.py
@@ -0,0 +1,8 @@
+"""
+This file is the starting point of the application.
+
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
--- api/run.py
+++ api/run.py
@@ -1,4 +1,10 @@
-from App import app
+"""
+File to run the flask api in one command
+
+"""
+
+from app import app
 
 if '__name__' == '__main__':
     app.run(debug=True)
+    
