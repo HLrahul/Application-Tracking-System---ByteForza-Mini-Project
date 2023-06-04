e4b085f1e63ec485ace49eaa7f5df2fd7bb80fb2
HLrahul <rahuls10org@gmail.com>
2023-06-04 14:15:01 +0530

    fetched `candidates` from `api`

--- client/src/pages/Candidates.tsx
+++ client/src/pages/Candidates.tsx
@@ -1,14 +1,30 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { P, CandidateWrapper, CandidatesList } from '../styles/Candidates.styles';
 import { Button } from '../styles/Common.styles';
+
+import { BASE_URL } from '../api/axios';
+import axios from '../api/axios';
 
 function Candidates() {
 
   const [candidatesList, setCandidatesList] = useState([]);
 
-  const navigate = useNavigate();
+  const getCandidates = async () => {
+    try {
+      const response = await axios.get(BASE_URL);
+      console.log(response);
+      setCandidatesList(response?.data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
+  useEffect(() => {
+    getCandidates();
+  }, []);
+
+  const navigate = useNavigate();
   function handleClick() {
     navigate("/addcandidate");
   }
