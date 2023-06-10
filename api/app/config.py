43c64ad19cbda619b5d5dbe42773bf4a31d10388
Rahul Kumar S <86195461+HLrahul@users.noreply.github.com>
2023-06-10 06:47:57 +0000

    setup `container` configs

--- api/app/config.py
+++ api/app/config.py
@@ -10,6 +10,5 @@ load_dotenv()
 
 DEBUG = True
 SECRET_KEY = os.environ.get('SECRET_KEY')
-SQLALCHEMY_DATABASE_URI = f"postgresql://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASSWORD')}@localhost/{os.environ.get('DB_NAME')}"
-
+SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@db:5432/application_tracking_system"
 RESUME_FOLDER = "/tmp"
